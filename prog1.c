aff1f2546192625e8a309c73777bbf776470b8e8
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2021-10-15 17:01:16 -0300

    add prog1/prog2 without changes

---
+++ prog1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <wait.h>
+#include <unistd.h>
+int main(void)
+{
+  int status, id, j;
+  *********Insira um comando para pegar o PID do processo corrente e mostre na tela da console.
+  
+  if (***insira um comando para criar um subprocesso){
+    *****Faça com que o processo pai execute este trecho de código
+    *****Mostre na console o PID do processo pai e do processo filho
+    *****Monte uma mensagem e a envie para o processo filho
+    *****Mostre na tela o texto da mensagem enviada
+    *****Aguarde a resposta do processo filho
+    *****Mostre na tela o texto recebido do processo filho
+    *****Aguarde mensagem do filho e mostre o texto recebido
+    *****Aguarde o término do processo filho
+    *****Informe na tela que o filho terminou e que o pai também vai encerrar
+  }else{
+    ***** Faça com que o processo filho execute este trecho de código
+    ***** Mostre na tela o PID do processo corrente e do processo pai
+    ***** Aguarde a mensagem do processo pai e ao receber mostre o texto na tela
+    ***** Envie uma mensagem resposta ao pai
+    ***** Execute o comando "for" abaixo
+  for (j = 0; j <= 10000; i++);
+  *****Envie mensagem ao processo pai com o valor final de "j"
+  *****Execute o comando abaixo e responda às perguntas
+  execl("/ Bin / ls", "ls", NULL);
+  *****O que acontece após este comando ? *****O que pode acontecer se o comando "execl" falhar ?
+  }
+  exit(0);
+}
---
+++ prog2.c
@@ -0,0 +1,21 @@
+Início
+  Lê linha de comando;
+  Enquanto não fim faça
+    Início
+      Percorre a linha retirando o nome do comando;
+      Executa um fork para criar um novo processo;
+      Se processo filho então
+        Executa execl especificando o nome do comando como parâmetro;
+      Senão
+        Inicio
+        Executa wait para esperar que a execução do comando termine;
+        Se codigo retorno = zero então
+          Escreva "Executado com sucesso." 
+        Senão
+          Escreva "Código de retorno = ", codigo_retorno;
+        Fim
+      Fim se;
+      Lê linha de comando;
+    Fim;
+
+Fim;
