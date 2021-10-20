95a5ba01fb1789d18ad562edbf9aadf2287aafb7
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2021-10-19 22:27:10 -0300

    clean code

--- prog1.c
+++ prog1.c
@@ -9,11 +9,11 @@
 int main(void)
 {
   int status, id, j;
-  int ret; //tem problema fazer aqui ao inves de dentro do if?
-  int mensageiroPaiParaFilho[2]; //declara o pipe
+  int ret;
+  int mensageiroPaiParaFilho[2];
   int mensageiroFilhoParaPai[2];
+  char *mensagem;
 
-  //Insira um comando para pegar o PID do processo corrente e mostre na tela da console.
   if(pipe(mensageiroPaiParaFilho) == -1){
     printf("deu ruim na mensagem do processo pai para o filho");
     return -1;
@@ -23,53 +23,49 @@ int main(void)
     return -1;
   }
 
-  //dentro do if -> insira um comando para criar um subprocesso
   if ((ret = fork()) > 0){
 
-    char str_buffer2[50];
-    //Faça com que o processo pai execute este trecho de código
-    //Mostre na console o PID do processo pai e do processo filho
-    printf("PID do processo pai: %d\n", getpid());
-    printf("PID do processo filho: %d\n", ret);
-    //Monte uma mensagem e a envie para o processo filho
-    char str[] = "mensagem para processo filho";
-
-    //fecha leitura do pipe
     close(mensageiroPaiParaFilho[READ]);
-
-    write(mensageiroPaiParaFilho[WRITE], str, sizeof(str));
-    //Mostre na tela o texto da mensagem enviada
-    //Aguarde a resposta do processo filho
-    //Mostre na tela o texto recebido do processo filho
-    //Aguarde mensagem do filho e mostre o texto recebido
-
     close(mensageiroFilhoParaPai[WRITE]);
-    read(mensageiroFilhoParaPai[READ], str_buffer2, sizeof(str_buffer2));
-    printf("string recebida do processo filho = %s \n", str_buffer2);
 
-    //Aguarde o término do processo filho
-    //Informe na tela que o filho terminou e que o pai também vai encerrar
+    mensagem = "mensagem para processo filho";
+    write(mensageiroPaiParaFilho[WRITE], &mensagem, sizeof(mensagem));
+
+    printf("Aguardando resposta do filho\n");
+    read(mensageiroFilhoParaPai[READ], &mensagem, sizeof(mensagem));
+    printf("------ PAI ------\nPID do processo pai: %d\nPID do processo filho: %d\n", getpid(),ret);
+    printf("string recebida do processo filho = %s \n", mensagem);
+
+    read(mensageiroFilhoParaPai[READ], &j, sizeof(j));
+    printf("O valor de 'j' segundo o filho é: %d \n", j);
+
+    wait(&status);
+
+    if(status == 0){
+      printf("O processo filho encerrou \n");
+    }else{
+      printf("O processo filho encerrou com falha \n");
+    }
+    printf("O processo pai encerrou\n");
   }else if(ret == 0){
-    // Faça com que o processo filho execute este trecho de código
-    // Mostre na tela o PID do processo corrente e do processo pai
-    printf("PID do processo pai: %d\n", getppid()); //pega pid do processo pai
-    printf("PID do processo filho: %d\n", getpid()); //pega pid do processo filho
+    close(mensageiroPaiParaFilho[WRITE]);
+    close(mensageiroFilhoParaPai[READ]);    
 
-    // Aguarde a mensagem do processo pai e ao receber mostre o texto na tela
-    close(mensageiroPaiParaFilho[WRITE]); //fecha escrita do pipe
-    char str_buffer[50];
-    read(mensageiroPaiParaFilho[READ], str_buffer, sizeof(str_buffer));
-    printf("string recebida do processo pai = %s \n", str_buffer);
-    // Envie uma mensagem resposta ao pai
-    char str2[] = "mensagem para processo pai\n";
-    close(mensageiroFilhoParaPai[READ]);
-    write(mensageiroFilhoParaPai[WRITE], str2, sizeof(str2));
-    // Execute o comando "for" abaixo
-  //for (j = 0; j <= 10000; j++);
-  //Envie mensagem ao processo pai com o valor final de "j"
-  //Execute o comando abaixo e responda às perguntas
-  //execl("/ Bin / ls", "ls", NULL);
-  //O que acontece após este comando ? *****O que pode acontecer se o comando "execl" falhar ?
+    read(mensageiroPaiParaFilho[READ], &mensagem, sizeof(mensagem));
+    printf("------ FILHO -------\nPID do processo pai: %d\nPID do processo filho: %d\n", getppid(), getpid());
+    printf("string recebida do processo pai = %s \n", mensagem);
+
+    mensagem = "mensagem para processo pai\n";
+    
+    write(mensageiroFilhoParaPai[WRITE], &mensagem, sizeof(mensagem));
+    for (j = 0; j <= 10000; j++);
+
+    write(mensageiroFilhoParaPai[WRITE], &j, sizeof(j));
+
+    execl("/bin/ls", "ls", NULL);
+    //O que acontece após este comando ? 
+    //O que pode acontecer se o comando "execl" falhar ?
   }
+
   exit(0);
 }
