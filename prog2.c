f4f484a00231d7b407d79f44d236461421beac12
Arthur Albuquerque <arthur.albuquerque.1995@gmail.com>
2021-10-19 23:18:30 -0300

    le linha de comando

--- prog2.c
+++ prog2.c
@@ -1,21 +1,36 @@
-/* Início
-  Lê linha de comando;
-  Enquanto não fim faça
-    Início
-      Percorre a linha retirando o nome do comando;
-      Executa um fork para criar um novo processo;
-      Se processo filho então
-        Executa execl especificando o nome do comando como parâmetro;
-      Senão
-        Inicio
-        Executa wait para esperar que a execução do comando termine;
-        Se codigo retorno = zero então
-          Escreva "Executado com sucesso." 
-        Senão
-          Escreva "Código de retorno = ", codigo_retorno;
-        Fim
-      Fim se;
-      Lê linha de comando;
-    Fim;
+#include <stdio.h>
+#include <stdlib.h>
+#include <wait.h>
+#include <unistd.h>
+#include <string.h>
 
-Fim; */
+int main(int argc, char *argv[]){
+  int status;
+  char *comando;
+  comando = argv[1];
+  char parametros[] = "";
+  for(int i = 2; i < argc; i++) {
+    strcat(parametros, argv[i]);
+    strcat(parametros, " ");
+  }
+  printf("%s \n", parametros);
+  //enquanto nao fim faça
+  while(1){
+    int ret = fork();
+
+    if (ret == 0){
+      printf("filho \n");
+      //usar o comando que foi lido anteriormente
+      execl(comando, comando, parametros, NULL);
+      printf("\n");
+    }else{
+      wait(&status);
+      if(status == 0){
+        printf("Executado com sucesso \n");
+      }else{
+        printf("Código de retorno = %d \n", status);
+      }
+    }
+    exit(0);
+  }
+}
